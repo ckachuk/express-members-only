2eb3359cd0764dd631d24765c5fe1e7145194eca
carlk <carloskachuk@gmail.com>
2022-07-28 15:31:11 -0300

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var mongoose = require("mongoose");
 require('dotenv').config();
 var indexRouter = require('./routes/index');
 require('./passport')
+var helmet = require('helmet');
+var compression = require('compression');
+
 
 var app = express();
 
@@ -19,10 +22,12 @@ db.on("error", console.error.bind(console, "mongo connection error"));
 
 
 
-app.use(cors())
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(compression());
+app.use(helmet());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -53,10 +58,9 @@ app.use(function(err, req, res, next) {
 
 
 
-var port = 5000
 
-app.listen(5000, function () {
-  console.log(' app listening on port ' + port + '!');
+app.listen(5000 , function () {
+  console.log(' app listening on port 5000 !');
 });
 
 
