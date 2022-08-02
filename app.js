c69d4d8f2a3b240e4c7293a3ba0a18f3a46f275d
carlk <carloskachuk@gmail.com>
2022-08-01 23:54:40 -0300

    remove errors in app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var createError = require('http-errors');
+
 var express = require('express');
 var cors = require('cors');
 var path = require('path');
@@ -36,24 +36,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: err
-  });
-});
 
 
 
