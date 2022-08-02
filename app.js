57aa397ebac508c1b048122b44c8363540534bc9
carlk <carloskachuk@gmail.com>
2022-08-01 22:01:19 -0300

    remove views in app.js

--- app.js
+++ app.js
@@ -31,9 +31,6 @@ app.use(helmet());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
 
 
